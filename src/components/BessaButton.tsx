86aa997ef6842fd5729981cf2bb0af842f9292d4
bessaapps <topher@stupendousweb.com>
2025-04-25 18:57:25 -0700

    Update version to 1.0.17 and move dependencies to devDependencies
    Bumped the version from 1.0.16 to 1.0.17 in package.json for a new release. Moved "react" and "react-native" from dependencies to devDependencies to better align with their usage in the project.

--- src/components/BessaButton.tsx
+++ src/components/BessaButton.tsx
@@ -1,9 +1,116 @@
-import { Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  useColorScheme,
+} from "react-native";
+// import { theme } from "../../utils/theme";
 
-export default function BessaButton() {
+const BessaButton = ({
+  anchor,
+  variant,
+  accessoryRight,
+  isLoading,
+  color = "neutral",
+  size = "medium",
+  isControl,
+  style,
+  containerStyle,
+  textStyle,
+  ...props
+}) => {
+  const colorScheme = useColorScheme();
+
   return (
-    <View>
-      <Text>from npm</Text>
+    <View style={[styles.container, { ...containerStyle }]}>
+      <TouchableOpacity
+        {...props}
+        style={[
+          styles.base,
+          styles[size],
+          {
+            // backgroundColor:
+            //   variant === "ghost"
+            //     ? "transparent"
+            //     : isControl
+            //       ? theme.colors[colorScheme].neutral[50]
+            //       : theme.colors[colorScheme][color][500],
+            // ...style,
+          },
+        ]}
+      >
+        <View style={styles.row}>
+          {anchor && (
+            <Text
+              style={[
+                styles.text,
+                {
+                  // color:
+                  //   variant === "ghost"
+                  //     ? theme.colors[colorScheme][color][950]
+                  //     : isControl
+                  //       ? theme.colors[colorScheme].neutral[950]
+                  //       : theme.colors[colorScheme][color][50],
+                  // fontSize:
+                  //   theme.spacings.md +
+                  //   (size === "small" ? -2 : size === "large" ? 2 : 0),
+                },
+                { ...textStyle },
+              ]}
+            >
+              {anchor}
+            </Text>
+          )}
+          {!isLoading && accessoryRight}
+        </View>
+        {isLoading && (
+          <ActivityIndicator
+            size={"small"}
+            // color={
+            //   variant === "ghost" || isControl
+            //     ? theme.colors[colorScheme][color][950]
+            //     : theme.colors[colorScheme][color][50]
+            // }
+          />
+        )}
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  // Standard Styles
+  container: {
+    // borderRadius: theme.radius.full,
+    overflow: "hidden",
+  },
+  base: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    // gap: theme.spacings.sm,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    // gap: theme.spacings.sm }
+  },
+  text: { fontFamily: "Inter_600SemiBold" },
+  // Sizes
+  small: {
+    // paddingVertical: theme.spacings.sm,
+    // paddingHorizontal: theme.spacings.md,
+  },
+  medium: {
+    // paddingVertical: theme.spacings.md,
+    // paddingHorizontal: theme.spacings.lg,
+  },
+  large: {
+    // paddingVertical: theme.spacings.lg,
+    // paddingHorizontal: theme.spacings.xl,
+  },
+});
+
+export default BessaButton;
