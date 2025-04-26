50ccfbcbbcbccbb6be118bcf9fe22e54a5d641e5
bessaapps <topher@stupendousweb.com>
2025-04-25 17:13:21 -0700

    Update version and add README

---
+++ src/components/BessaButton.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const BessaButton = () => {
+  return (
+    <View>
+      <Text>im a button</Text>
+    </View>
+  );
+};
+
+export default BessaButton;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as BessaButton } from "./components/BessaButton";
